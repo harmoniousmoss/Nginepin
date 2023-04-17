4ff09c43449cdea01d7b2c5a5a93bd85a186ac8b
Syaeful Bahri <syaefulbahri@protonmail.com>
2023-04-18 05:24:08 +0700

    Ganti warna Text

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div className="text-2xl  text-rose-500">Hello Nginepin</div>;
+  return <div className="text-2xl text-indigo-700">Hello Nginepin</div>;
 }
