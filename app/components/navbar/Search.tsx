bb0da75094d9d55277974f02370924f6dbeef1a8
Syaeful Bahri <99127787+harmoniousmoss@users.noreply.github.com>
2023-04-18 07:06:30 +0700

    Fix search item alignment

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -5,7 +5,7 @@ import { BiSearch } from "react-icons/bi"
 const Search = () => {
   return (
     <div className="border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer">
-      <div className="flex flex-row items-start justify-between">
+      <div className="flex flex-row items-center justify-between">
         <div className="px-6 text-sm font-semibold">Anywhere</div>
         <div className="hidden px-6 text-sm font-semibold border-x-[1px] sm:block flex-1 text-center">
           Any Week
